a30356e68cf70e0d80dd2e105ecd6d3ac09501c5
Evan Kelley <kelleyev@mail.uc.edu>
2021-11-07 16:19:42 -0800

    Modified Controller

---
+++ Assignment8/Controllers/NameReturn.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment8.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameReturn : ControllerBase
+    {
+     
+
+        private readonly ILogger<NameReturn> _logger;
+
+        public NameReturn(ILogger<NameReturn> logger)
+        {
+            _logger = logger;
+        }
+
+       
+
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+        return { "name":"Evan Kelley"}
+        }
+    }
+}
---
+++ Assignment8/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment8
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        
+    }
+}
